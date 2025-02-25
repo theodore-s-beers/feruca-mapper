43c2caf171b87c593cdf0bc2af6f01a4ee4aeff0
Theo Beers <32523293+theodore-s-beers@users.noreply.github.com>
2025-02-24 20:44:42 -0500

    Add Persian characters

--- src/arabic_interleaved.rs
+++ src/arabic_interleaved.rs
@@ -15,19 +15,23 @@ static MAPPING: LazyLock<HashMap<u16, u16>> = LazyLock::new(|| {
         (0x2A6E, 0x2383 + SHIFT),        // Alif hamza below
         (0x2A76, 0x2384 + SHIFT),        // Alif
         (0x2A78, 0x239B + BUMP + SHIFT), // Ba
+        (0x2A97, 0x23B5 + BUMP + SHIFT), // Che
         (0x2AA9, 0x23CB + BUMP + SHIFT), // Dal
         (0x2AAA, 0x23CC + BUMP + SHIFT), // Dhal
         (0x2AD8, 0x23CD + BUMP + SHIFT), // Ḍ
         (0x2AED, 0x2423 + BUMP + SHIFT), // Fa
         (0x2AE5, 0x2432 + BUMP + SHIFT), // Gh
+        (0x2B0A, 0x2433 + BUMP + SHIFT), // Gaf
         (0x2A9E, 0x2459 + SHIFT),        // Ḥ
         (0x2B30, 0x245A + SHIFT),        // Ha
         (0x2A93, 0x2490 + SHIFT),        // Jim
         (0x2A9F, 0x24A9 + SHIFT),        // Kh
         (0x2B00, 0x24AA + SHIFT),        // Kaf
+        (0x2B01, 0x24AB + SHIFT),        // Kaf (Persian)
         (0x2B19, 0x24BD + SHIFT),        // Lam
         (0x2B21, 0x24F7 + SHIFT),        // Mim
         (0x2B25, 0x2506 + SHIFT),        // Nun
+        (0x2A7A, 0x255D + SHIFT),        // Pe
         (0x2AF9, 0x2572 + SHIFT),        // Qaf
         (0x2AB9, 0x2585 + SHIFT),        // Ra
         (0x2ACC, 0x25C7 + SHIFT),        // Sin
@@ -38,8 +42,10 @@ static MAPPING: LazyLock<HashMap<u16, u16>> = LazyLock::new(|| {
         (0x2ADD, 0x25F4 + SHIFT),        // Ṭ
         (0x2B36, 0x2657 + SHIFT),        // Waw
         (0x2B45, 0x266D + SHIFT),        // Ya
+        (0x2B46, 0x266E + SHIFT),        // Ya (Persian)
         (0x2ABA, 0x2683 + SHIFT),        // Za
-        (0x2ADE, 0x2684 + SHIFT),        // Ẓ
+        (0x2AC2, 0x2684 + SHIFT),        // Zhe
+        (0x2ADE, 0x2685 + SHIFT),        // Ẓ
     ])
 });
 
